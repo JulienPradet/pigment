36d855f833ae669d48a5efde4839cf98d1f0c9d2
Julien <julien.pradet@gmail.com>
2018-03-10 15:03:57 +0100

    Add rxjs fs utilities
    Since the whole build pipeline will be in rxjs, this lib makes
    it easier to read/write files.

---
+++ packages/pigment-fs/index.js
@@ -0,0 +1,156 @@
+const { Observable } = require("rxjs/Observable");
+const { mergeMap, map, expand, filter } = require("rxjs/operators");
+const fs = require("fs");
+const path = require("path");
+const mkdirpLib = require("mkdirp");
+const chokidar = require("chokidar");
+
+function exists(filePath) {
+  return Observable.create(observer => {
+    fs.exists(filePath, exists => {
+      observer.next(exists);
+      observer.complete();
+    });
+  });
+}
+
+function watchfile(filePath) {
+  return Observable.create(observer => {
+    chokidar.watch(filePath, { persistent: true }).on("change", () => {
+      observer.next();
+    });
+  });
+}
+
+function readdir(dirPath) {
+  return Observable.create(observer => {
+    fs.readdir(dirPath, (e, files) => {
+      if (e) return observer.error(e);
+
+      files = files.map(file => path.join(dirPath, file));
+      observer.next(files);
+      observer.complete();
+    });
+  });
+}
+
+function readfile(filepath) {
+  return Observable.create(observer => {
+    fs.readFile(filepath, (e, file) => {
+      if (e) return observer.error(e);
+
+      const data = { filepath: filepath, file: file.toString() };
+      observer.next(data);
+      observer.complete();
+    });
+  });
+}
+
+function writefile(filePath, content, options = {}) {
+  return Observable.create(observer => {
+    fs.writeFile(filePath, content, options, e => {
+      if (e) return observer.error(e);
+
+      observer.next(filePath);
+      observer.complete();
+    });
+  });
+}
+
+function stat(filePath) {
+  return Observable.create(observer => {
+    fs.stat(filePath, (e, stats) => {
+      if (e) return observer.error(e);
+
+      observer.next({ filepath: filePath, stats });
+      observer.complete();
+    });
+  });
+}
+
+function mkdirp(path) {
+  return Observable.create(observer => {
+    mkdirpLib(path, e => {
+      if (e) return observer.error(e);
+
+      observer.next(path);
+      observer.complete();
+    });
+  });
+}
+
+function getRecursiveFiles(inputDir$) {
+  return inputDir$.pipe(
+    mergeMap(dirpath => readdir(dirpath)),
+    mergeMap(files => files), // flatten all files
+    mergeMap(filepath => stat(filepath)),
+    map(({ filepath, stats }) => ({
+      filepath,
+      stats,
+      isDirectory: stats.isDirectory()
+    })),
+    expand(
+      ({ filepath, stats, isDirectory }) =>
+        isDirectory
+          ? getRecursiveFiles(Observable.of(filepath))
+          : Observable.empty()
+    ),
+    filter(({ isDirectory }) => !isDirectory),
+    map(({ isDirectory, filepath, stats }) => ({ filepath, stats }))
+  );
+}
+
+function saveFiles(filesToSave$) {
+  return filesToSave$.pipe(
+    mergeMap(({ file, filepath }) => mkdirp(path.dirname(filepath))),
+    map(() => ({ file, filepath })),
+    mergeMap(({ file, filepath }) => writefile(filepath, file))
+  );
+}
+
+function copyfile(sourcePath, destPath, recursive = false) {
+  let file$;
+  if (recursive) {
+    file$ = getRecursiveFiles(Observable.of(sourcePath)).pipe(
+      mergeMap(({ filepath }) =>
+        copyfile(
+          filepath,
+          path.join(destPath, path.relative(sourcePath, filepath))
+        )
+      )
+    );
+  } else {
+    file$ = mkdirp(path.dirname(destPath)).pipe(
+      mergeMap(() =>
+        Observable.create(observer => {
+          const readStream = fs.createReadStream(sourcePath);
+          const writeStream = fs.createWriteStream(destPath);
+
+          writeStream.on("close", () => {
+            observer.next(destPath);
+            observer.complete();
+          });
+          writeStream.on("error", err => observer.error(err));
+          readStream.on("error", err => observer.error(err));
+
+          readStream.pipe(writeStream);
+        })
+      )
+    );
+  }
+
+  return file$;
+}
+
+module.exports = {
+  exists,
+  watchfile,
+  readdir,
+  readfile,
+  writefile,
+  stat,
+  mkdirp,
+  getRecursiveFiles,
+  saveFiles,
+  copyfile
+};
