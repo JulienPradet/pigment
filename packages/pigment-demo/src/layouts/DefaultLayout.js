747f341eb6c69131e12584f14f098cc3dcff330a
Julien <julien.pradet@gmail.com>
2018-03-23 11:26:36 +0100

    Remove warning when rendering the default layout

--- packages/pigment-demo/src/layouts/DefaultLayout.js
+++ packages/pigment-demo/src/layouts/DefaultLayout.js
@@ -2,18 +2,16 @@ import React from "react";
 import Header from "../modules/Header";
 import Footer from "../modules/Footer";
 
-class DefaultLayout extends React.Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <hr />
-        {this.props.children}
-        <hr />
-        <Footer />
-      </div>
-    );
-  }
-}
+const DefaultLayout = props => {
+  return (
+    <div>
+      <Header />
+      <hr />
+      {props.children}
+      <hr />
+      <Footer />
+    </div>
+  );
+};
 
 export default DefaultLayout;
