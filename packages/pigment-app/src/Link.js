22ebf7fb963f9d58b492a7f33401eac2e7e84419
Julien <julien.pradet@gmail.com>
2018-03-23 11:25:44 +0100

    Setup a very basic implementation of the Router
    The page is no longer refreshed on a route change

--- packages/pigment-app/src/Link.js
+++ packages/pigment-app/src/Link.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { RouterContext } from "./Router";
 
 const Link = ({ to, children }) => {
-  return <a href={to}>{children}</a>;
+  return (
+    <RouterContext.Consumer>
+      {({ push }) => (
+        <a
+          href={to}
+          onClick={event => {
+            event.preventDefault();
+            push(to);
+          }}
+        >
+          {children}
+        </a>
+      )}
+    </RouterContext.Consumer>
+  );
 };
 
 export default Link;
--- packages/pigment-app/src/Router.js
+++ packages/pigment-app/src/Router.js
@@ -1,4 +1,12 @@
-import { Component } from "react";
+import React, { Component, createContext } from "react";
+
+const toRoute = route => {
+  if (typeof route === "string") {
+    return { pathname: route };
+  } else {
+    return route;
+  }
+};
 
 const getPage = (route, pages) => {
   const page = pages.find(page => page.pathname === route.pathname);
@@ -9,17 +17,50 @@ const getPage = (route, pages) => {
   }
 };
 
+export const RouterContext = createContext("router");
+
 class Router extends Component {
   constructor(props) {
     super();
     this.state = {
       page: getPage(props.initialRoute, props.pages)
     };
+    this.handlePopState = this.handlePopState.bind(this);
+    this.push = this.push.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("popstate", this.handlePopState);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("popstate", this.handlePopState);
+  }
+
+  handlePopState(event) {
+    if (event.isTrusted) {
+      const pathname = window.location.pathname;
+      this.setState({
+        page: getPage(toRoute(pathname), this.props.pages)
+      });
+    }
+  }
+
+  push(route) {
+    route = toRoute(route);
+    window.history.pushState({}, null, route.pathname);
+    this.setState({
+      page: getPage(route, this.props.pages)
+    });
   }
 
   render() {
     const page = this.state.page;
-    return this.props.children(page);
+    return (
+      <RouterContext.Provider value={{ push: this.push }}>
+        {this.props.children(page)}
+      </RouterContext.Provider>
+    );
   }
 }
 
