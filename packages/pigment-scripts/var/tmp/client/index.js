50ea0d71ce91f291479e847a4a6bf151278bf0cb
Julien <julien.pradet@gmail.com>
2018-03-25 10:58:03 +0200

    Remove temporary files from git

--- packages/pigment-scripts/var/tmp/client/index.js
+++
@@ -1,4 +0,0 @@
-import App from "../app/index.js";
-import { clientRenderApp } from "../../../src/serve/clientRenderApp";
-
-clientRenderApp(App);
--- packages/pigment-scripts/var/tmp/server/index.js
+++
@@ -1,5 +0,0 @@
-import App from "./../app/index.js";
-import Document from "./../../../../pigment-demo/src/Document.js";
-import { serverRenderApp } from "../../../src/serve/serverRenderApp";
-
-export default serverRenderApp(Document, App);
