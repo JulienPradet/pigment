f16bfaec8a32e19c72ed44f47fb5639f98373d67
Julien <julien.pradet@gmail.com>
2018-05-03 10:22:11 -0400

    Use programmatical interface for lighthouse

--- packages/pigment-scripts/scripts/test.perf.js
+++ packages/pigment-scripts/scripts/test.perf.js
@@ -1,8 +1,10 @@
-const { spawn } = require("child_process");
 const path = require("path");
 const paths = require("../config/paths");
 const log = require("@pigment/log")("PERF");
 const { stripIndent } = require("common-tags");
+const lighthouse = require("lighthouse");
+const chromeLauncher = require("chrome-launcher");
+const perfConfig = require("lighthouse/lighthouse-core/config/perf.json");
 
 const budget = {
   performance: 98
@@ -13,58 +15,58 @@ module.exports = () => {
 
   const paths = require("../config/paths")();
   log.message("info", "Launching example server...");
-  const server = require("../src/serve/serve.prod")(paths, () => {
+  const server = require("../src/serve/serve.prod")(paths, async () => {
+    log.message("info", "Launching browser...");
+
+    const opts = {
+      chromeFlags: ["--headless"]
+    };
+    const chrome = await chromeLauncher.launch(opts);
+
     log.message("info", "Calculating scores...");
-    const lighthouse = spawn(
-      "lighthouse",
-      [
-        "--perf",
-        "--output=json",
-        "--save-assets",
-        '--chrome-flags="--no-sandbox --headless --ignore-certificate-errors"',
-        "http://localhost:3000/",
-        "--output-path=./build/stats/lighthouse.json"
-      ],
-      { stdio: "inherit" }
+    const result = await lighthouse(
+      "http://localhost:3000/",
+      {
+        ...opts,
+        port: chrome.port
+      },
+      perfConfig
     );
+    chrome.kill();
 
-    lighthouse.on("close", () => {
-      const result = require(path.join(paths.buildStats, "lighthouse.json"));
-
-      const audits = result.audits;
-      const failedAudits = Object.keys(audits).filter(
-        name => audits[name].score !== true && audits[name].score !== 100
-      );
-      failedAudits.forEach(name => {
-        const { helpText, description, score, displayValue } = audits[name];
-        log.message(
-          "warn",
-          stripIndent`
+    const audits = result.audits;
+    const failedAudits = Object.keys(audits).filter(
+      name => audits[name].score !== true && audits[name].score !== 100
+    );
+    failedAudits.forEach(name => {
+      const { helpText, description, score, displayValue } = audits[name];
+      log.message(
+        "warn",
+        stripIndent`
             ${description}: ${displayValue}
             Score: ${score}
             ${helpText}
           `
-        );
-      });
+      );
+    });
 
-      if (result.score < budget.performance) {
-        log.message(
-          "error",
-          stripIndent`
+    if (result.score < budget.performance) {
+      log.message(
+        "error",
+        stripIndent`
             Performance budget failed! (Score: ${result.score.toFixed(1)})
             Expected >= ${budget.performance}
           `
-        );
-        server.close();
-        process.exit(1);
-      } else {
-        log.message(
-          "success",
-          `Performance budget achieved! (Score: ${result.score.toFixed(1)})`
-        );
-        process.exit(0);
-      }
-    });
+      );
+      server.close();
+      process.exit(1);
+    } else {
+      log.message(
+        "success",
+        `Performance budget achieved! (Score: ${result.score.toFixed(1)})`
+      );
+      process.exit(0);
+    }
   });
 
   process.on("SIGINT", () => {
