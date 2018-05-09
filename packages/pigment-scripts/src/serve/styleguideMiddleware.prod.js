59f630c1d9af84a382f2dfa455186f5a0340ab60
Julien <julien.pradet@gmail.com>
2018-05-09 16:09:45 -0400

    Serve mock graphql server in production mode

--- packages/pigment-scripts/src/serve/styleguideMiddleware.prod.js
+++ packages/pigment-scripts/src/serve/styleguideMiddleware.prod.js
@@ -7,6 +7,10 @@ module.exports = paths => {
     paths.build,
     "stats/stats.styleguide.json"
   ));
+  const serverStats = require(path.join(
+    paths.build,
+    "stats/stats.server.json"
+  ));
 
   const script = "/" + styleguideStats.assetsByChunkName.styleguide[0];
 
@@ -27,7 +31,19 @@ module.exports = paths => {
     };
   };
 
+  const makeStyleguideGraphQLMiddleware = () => {
+    const styleguideGraphQLFilename = path.join(
+      paths.buildServer,
+      serverStats.assetsByChunkName.styleguideGraphQL[0]
+    );
+    const styleguideGraphQLMiddleware = require(styleguideGraphQLFilename)
+      .default;
+
+    return styleguideGraphQLMiddleware;
+  };
+
   const router = express.Router();
+  router.use("/_pigment/styleguide/", makeStyleguideGraphQLMiddleware());
   router.use("/_pigment/styleguide*", makeStyleguideMiddleware());
   return router;
 };
