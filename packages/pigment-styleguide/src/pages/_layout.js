f3f71f2d33a3ab2487ae82eda9d7f9661a35b6c2
Julien <julien.pradet@gmail.com>
2018-05-19 12:17:09 +0200

    Update layout with newer navigation

--- packages/pigment-styleguide/src/pages/_layout.js
+++ packages/pigment-styleguide/src/pages/_layout.js
@@ -1,11 +1,33 @@
 import React from "react";
-import Nav from "../modules/Nav";
+import { css, injectGlobal } from "emotion";
+import Navigation from "../modules/Navigation";
+
+injectGlobal`
+  body {
+    margin: 0;
+  }
+`;
+
+const layoutClass = css`
+  display: flex;
+  min-height: 100vh;
+`;
+const navClass = css`
+  background: #e6edd2;
+  padding: 1em;
+`;
+const contentClass = css`
+  margin: 1em;
+`;
 
 const StyleguideLayout = ({ children }) => {
   return (
-    <div>
-      <Nav />
-      {children}
+    <div className={layoutClass}>
+      <div className={navClass}>
+        <h1>Pigment Store</h1>
+        <Navigation />
+      </div>
+      <div className={contentClass}>{children}</div>
     </div>
   );
 };
